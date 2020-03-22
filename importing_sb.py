eb0b7b461c11e6b6430ef2f8a1b9e3b20f820122
Jonathan Manuel <jonathanpmanuel@gmail.com>
2020-03-22 17:10:04 +0000

    Created post_pred notebook which calculates post-match result probabilities based off xG. Work on pass_sonar so it now works and basis for informative plots.

--- importing_sb.py
+++ importing_sb.py
@@ -1032,52 +1032,52 @@ def draw_pitch(pitch_col, line_col, orientation,view):
         lx1 = [0,120,120,0,0]
         ly1 = [0,0,80,80,0]
         
-        plt.plot(lx1,ly1,color=line_col,zorder=5)
+        plt.plot(lx1,ly1,color=line_col,zorder=1)
         
         # boxes, 6 yard box and goals
         
         #outer boxes#
         lx2 = [120,102,102,120]
         ly2 = [18,18,62,62] 
-        plt.plot(lx2,ly2,color=line_col,zorder=5)
+        plt.plot(lx2,ly2,color=line_col,zorder=1)
         
         lx3 = [0,18,18,0]
         ly3 = [18,18,62,62] 
-        plt.plot(lx3,ly3,color=line_col,zorder=5)
+        plt.plot(lx3,ly3,color=line_col,zorder=1)
         
         #goals#
-        lx4 = [120,120.2,120.2,120]
+        lx4 = [120,120.4,120.4,120]
         ly4 = [36,36,44,44]
-        plt.plot(lx4,ly4,color=line_col,zorder=5)
+        plt.plot(lx4,ly4,color=line_col,zorder=1)
         
         lx5 = [0,-0.4,-0.4,0]
         ly5 = [36,36,44,44]
-        plt.plot(lx5,ly5,color=line_col,zorder=5)
+        plt.plot(lx5,ly5,color=line_col,zorder=1)
         
         #6 yard boxes#
         lx6 = [120,114,114,120]
         ly6 = [30,30,50,50]
-        plt.plot(lx6,ly6,color=line_col,zorder=5)
+        plt.plot(lx6,ly6,color=line_col,zorder=1)
         
         lx7 = [0,6,6,0]
         ly7 = [30,30,50,50]
-        plt.plot(lx7,ly7,color=line_col,zorder=5)
+        plt.plot(lx7,ly7,color=line_col,zorder=1)
         
         #Halfway line, penalty spots, and kickoff spot
         lx8 = [60,60]
         ly8 = [0,80]
-        plt.plot(lx8,ly8,color=line_col,zorder=5)
+        plt.plot(lx8,ly8,color=line_col,zorder=1)
         
-        plt.scatter(108,40,color=line_col,zorder=5) # pen right
-        plt.scatter(12,40,color=line_col,zorder=5) # pen left
-        plt.scatter(60,40,color=line_col,zorder=5) # kickoff
+        plt.scatter(108,40,color=line_col,zorder=1) # pen right
+        plt.scatter(12,40,color=line_col,zorder=1) # pen left
+        plt.scatter(60,40,color=line_col,zorder=1) # kickoff
         
         #Circles
         centreCircle = plt.Circle((60,40),10,lw=1.5,color=line_col,fill=False,zorder=1,alpha=1)
             
         #Arcs
-        rightArc = Arc((108,40),height=20,width=20,angle=0,theta1=127,theta2=233,color="black",lw=1.5)
-        leftArc = Arc((12,40),height=20,width=20,angle=0,theta1=307,theta2=53,color="black",lw=1.5)
+        rightArc = Arc((108,40),height=20,width=20,angle=0,theta1=127,theta2=233,color=line_col,lw=1.5)
+        leftArc = Arc((12,40),height=20,width=20,angle=0,theta1=307,theta2=53,color=line_col,lw=1.5)
         
         ax.add_patch(centreCircle)
         ax.add_patch(rightArc)
@@ -1143,8 +1143,8 @@ def draw_pitch(pitch_col, line_col, orientation,view):
         centreCircle = plt.Circle((40,60),10,lw=1.5,color=line_col,fill=False,zorder=1,alpha=1)
             
         #Arcs
-        rightArc = Arc((40,108),height=20,width=20,angle=90,theta1=127,theta2=233,color="black",lw=1.5)
-        leftArc = Arc((40,12),height=20,width=20,angle=90,theta1=307,theta2=53,color="black",lw=1.5)
+        rightArc = Arc((40,108),height=20,width=20,angle=90,theta1=127,theta2=233,color=line_col,lw=1.5)
+        leftArc = Arc((40,12),height=20,width=20,angle=90,theta1=307,theta2=53,color=line_col,lw=1.5)
         
         ax.add_patch(centreCircle)
         ax.add_patch(rightArc)
